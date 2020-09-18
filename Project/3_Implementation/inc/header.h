e93151f59b48b3e032351292585879a0ffc78f27
99002687 <71481417+99002687@users.noreply.github.com>
2020-09-18 17:14:58 +0530

    Update header.h

--- Project/3_Implementation/inc/header.h
+++ Project/3_Implementation/inc/header.h
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+struct student
+{
+ int rollno;
+ char name[30];
+ float mark;
+}stud;
+void insert()
+ void disp() 
+  void search()
+  void deletefile()
+  void update()
+  void sort()
+  int avlrollno(int rno)
+  int empty()
